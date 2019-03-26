6ab62bfd202ac75c8a77ef1fb1d31500bf1ba023
xm94630 <xm94630@126.com>
2019-03-26 19:13:30 +0800

    样式

--- src/pages/indexPage.vue
+++ src/pages/indexPage.vue
@@ -8,25 +8,62 @@
       </div>
 
       <div class="totalBox">
-        <el-row :gutter="50">
-          <el-col :span="6"><div class="lineBox totalBoxL">123</div></el-col>
-          <el-col :span="18">
+        <el-row :gutter="30">
+          <el-col :span="8"><div class="borderBox totalBoxL">123</div></el-col>
+          <el-col :span="16">
             <div class="totalBoxR">
               <div class="totalBoxRT">
-                <div>上海会员物流</div>
-                <div>拣货进度</div>
+                <div>上海会员物流拣货进度</div>
                 <div class="dateBox">1970-01-01 18:00:00</div>
               </div>
               <div class="totalBoxRB">
-                <el-row :gutter="50">
-                  <el-col :span="8"><div class="lineBox">1</div></el-col>
-                  <el-col :span="8"><div class="lineBox">2</div></el-col>
-                  <el-col :span="8"><div class="lineBox">3</div></el-col>
+                <el-row :gutter="30">
+                  <el-col :span="8"><div class="borderBox">1</div></el-col>
+                  <el-col :span="8"><div class="borderBox">2</div></el-col>
+                  <el-col :span="8"><div class="borderBox">3</div></el-col>
                 </el-row>
               </div>
             </div>
           </el-col>
         </el-row>
+      </div>
+      
+      <div class="linesBox borderBox MT10">
+        <div class="linesList">
+          <el-row :gutter="10">
+            <el-col :span="1"><div class="borderBox">1</div></el-col>
+            <el-col :span="11"><div class="borderBox">2</div></el-col>
+            <el-col :span="12"><div class="borderBox">3</div></el-col>
+          </el-row>
+        </div>
+        <div class="linesList">
+          <el-row :gutter="10">
+            <el-col :span="1"><div class="borderBox">1</div></el-col>
+            <el-col :span="11"><div class="borderBox">2</div></el-col>
+            <el-col :span="12"><div class="borderBox">3</div></el-col>
+          </el-row>
+        </div>
+        <div class="linesList">
+          <el-row :gutter="10">
+            <el-col :span="1"><div class="borderBox">1</div></el-col>
+            <el-col :span="11"><div class="borderBox">2</div></el-col>
+            <el-col :span="12"><div class="borderBox">3</div></el-col>
+          </el-row>
+        </div>
+        <div class="linesList">
+          <el-row :gutter="10">
+            <el-col :span="1"><div class="borderBox">1</div></el-col>
+            <el-col :span="11"><div class="borderBox">2</div></el-col>
+            <el-col :span="12"><div class="borderBox">3</div></el-col>
+          </el-row>
+        </div>
+        <div class="linesList">
+          <el-row :gutter="10">
+            <el-col :span="1"><div class="borderBox">1</div></el-col>
+            <el-col :span="11"><div class="borderBox">2</div></el-col>
+            <el-col :span="12"><div class="borderBox">3</div></el-col>
+          </el-row>
+        </div>
       </div>
 
   </div>
@@ -75,16 +112,16 @@ export default {
   }
 }
 .totalBox{
-  height:260px;
+  height:160px;
   width:100%;
   .totalBoxL{
-    height:260px;
+    height:160px;
   }
   .totalBoxR{
-    height:260px;
+    height:160px;
     .totalBoxRT{
-      height:160px;
-      font-size:40px;
+      height:70px;
+      font-size:20px;
       text-align:center;
       position:relative;
       .dateBox{
@@ -95,14 +132,25 @@ export default {
       }
     }
     .totalBoxRB{
-      .lineBox{
-        height:100px;
+      .borderBox{
+        height:80px;
       }
     }
   }
 }
-.lineBox{
+.borderBox{
   border:solid 1px #666;
+}
+
+.linesBox{
+  background:#fff;
+  height:460px;
+  .borderBox{
+    height:75px;
+  }
+}
+.linesList:nth-child(odd){
+  background:#f3f3f3;
 }
 
 
