6020e1d62a802b166f64a14c396090636f3b5c95
xm94630 <xm94630@126.com>
2019-04-02 16:00:28 +0800

    mod

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,379 +13,379 @@ router.get('/string', async (ctx, next) => {
 
 //接口1
 router.get('/epimetheus/api/diy/report/queryReportByCode', async (ctx, next) => {
-  
+
   //页面1接口
   ctx.body = {
 
-    total:{
-      rate:{data:0.5},
-      SKU:{
-        finish:12,
-        total:32,
+    total: {
+      rate: { data: 0.5 },
+      SKU: {
+        finish: 12,
+        total: 32,
       },
-      E:{
-        finish:345,
-        total:2345,
+      E: {
+        finish: 345,
+        total: 2345,
       },
-      shop:{
-        finish:12,
-        total:23,
+      shop: {
+        finish: 12,
+        total: 23,
       },
     },
-  
-    lines:[
+
+    lines: [
       {
-        SKU:{
-          finish:234,
-          total:1222,
+        SKU: {
+          finish: 234,
+          total: 1222,
         },
-        E:{
-          finish:17,
-          total:32,
+        E: {
+          finish: 17,
+          total: 32,
         },
-        shop:{
-          finish:34,
-          total:23,
+        shop: {
+          finish: 34,
+          total: 23,
         },
-        shopState:[{
-          name:'绍兴店',
-          state:0,
-        },{
-          name:'南京店',
-          state:1,
-        },{
-          name:'北京店',
-          state:2,
-        },{
-          name:'广州店',
-          state:2,
-        }], 
-        lineName:'线路1'
+        shopState: [{
+          name: '绍兴店',
+          state: 0,
+        }, {
+          name: '南京店',
+          state: 1,
+        }, {
+          name: '北京店',
+          state: 2,
+        }, {
+          name: '广州店',
+          state: 2,
+        }],
+        lineName: '线路1'
       },
       {
-        SKU:{
-          finish:55,
-          total:438,
+        SKU: {
+          finish: 55,
+          total: 438,
         },
-        E:{
-          finish:1,
-          total:233,
+        E: {
+          finish: 1,
+          total: 233,
         },
-        shop:{
-          finish:1,
-          total:432,
+        shop: {
+          finish: 1,
+          total: 432,
         },
-        shopState:[{
-          name:'大兴店',
-          state:1,
+        shopState: [{
+          name: '大兴店',
+          state: 1,
         }],
-        lineName:'线路2' 
-        
+        lineName: '线路2'
+
       }
 
     ]
 
-    
+
   }
 
 })
 
 //接口2
 router.get('/epimetheus/api/diy/report/queryReportByCode/2', async (ctx, next) => {
-  
+
   ctx.body = {
 
-    total:{
-  
-      taskTotal:{
-  
-        SKU:10,
-  
-        E:22,
-  
+    total: {
+
+      taskTotal: {
+
+        SKU: 10,
+
+        E: 22,
+
       },
-  
-      finish:{
-  
-        SKU:1,
-  
-        E:2,
-  
+
+      finish: {
+
+        SKU: 1,
+
+        E: 2,
+
       },
-  
-      unfinish:{
-  
-        SKU:3,
-  
-        E:4,
-  
+
+      unfinish: {
+
+        SKU: 3,
+
+        E: 4,
+
       },
-  
-      abnormal :{
-  
-        SKU:5,
-  
-        E:6,
-  
+
+      abnormal: {
+
+        SKU: 5,
+
+        E: 6,
+
       },
-  
-      pie:[100,200,300] //已完成、未完成、异常
-  
+
+      pie: [100, 200, 300] //已完成、未完成、异常
+
     },
-  
-    DCReplenish:{
-  
-      taskTotal:{
-  
-        SKU:2,
-  
-        E:3,
-  
+
+    DCReplenish: {
+
+      taskTotal: {
+
+        SKU: 2,
+
+        E: 3,
+
       },
-  
-      finish:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      finish: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      unfinish:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      unfinish: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      abnormal :{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      abnormal: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-  pie:[500,100,700] //已完成、未完成、异常
-  
+
+      pie: [500, 100, 700] //已完成、未完成、异常
+
     },
-  
-    AGVPicking:{
-  
-      taskTotal:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+    AGVPicking: {
+
+      taskTotal: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      finish:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      finish: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      unfinish:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      unfinish: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      abnormal :{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      abnormal: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-  pie:[100,200,300] //已完成、未完成、异常
-  
+
+      pie: [100, 200, 300] //已完成、未完成、异常
+
     },
-  
-    DCPicking:{
-  
-      taskTotal:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+    DCPicking: {
+
+      taskTotal: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      finish:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      finish: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      unfinish:{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      unfinish: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-      abnormal :{
-  
-        SKU:888,
-  
-        E:8888,
-  
+
+      abnormal: {
+
+        SKU: 888,
+
+        E: 8888,
+
       },
-  
-       pie:[100,200,300] //已完成、未完成、异常
-  
+
+      pie: [100, 200, 300] //已完成、未完成、异常
+
     },
-  
+
   }
 
 })
 
 //接口3
 router.get('/epimetheus/api/diy/report/queryReportByCode/3', async (ctx, next) => {
-  
+
   ctx.body = {
 
-    total:{
-  
-      data:{
-  
+    total: {
+
+      data: {
+
         title: {
-  
+
           text: '当日拣货任务汇总',
-  
+
           subtext: ''
-  
+
         },
-  
-        xAxis:{
-  
-          data:['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
-  
+
+        xAxis: {
+
+          data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
+
         },
-  
-        series:[
-  
+
+        series: [
+
           {
-  
-            name:'2011',
-  
-            data:[10, 52, 200, 334, 390, 330, 220],
-  
+
+            name: '2011',
+
+            data: [10, 52, 200, 334, 390, 330, 220],
+
           },
-  
+
           {
-  
-            name:'2012',
-  
-            data:[10, 52, 200, 334, 390, 330, 220],
-  
+
+            name: '2012',
+
+            data: [10, 52, 200, 334, 390, 330, 220],
+
           }
-  
+
         ]
-  
+
       }
-  
+
     },
-  
-    people:{
-  
-      name:"人工拣货每人拣货量",
-  
-      data:{
-  
+
+    people: {
+
+      name: "人工拣货每人拣货量",
+
+      data: {
+
         title: {
-  
+
           text: '当日拣货任务汇总',
-  
+
           subtext: ''
-  
+
         },
-  
-        xAxis:{
-  
-          data:['甲', '乙', '丙', '丁', '甲', '甲', '甲'],
-  
+
+        xAxis: {
+
+          data: ['甲', '乙', '丙', '丁', '甲', '甲', '甲'],
+
         },
-  
-        series:[
-  
+
+        series: [
+
           {
-  
-            name:'2011',
-  
-            data:[10, 52, 200, 334, 390, 330, 220],
-  
+
+            name: '2011',
+
+            data: [10, 52, 200, 334, 390, 330, 220],
+
           },
-  
+
           {
-  
-            name:'2012',
-  
-            data:[10, 52, 200, 334, 390, 330, 220],
-  
+
+            name: '2012',
+
+            data: [10, 52, 200, 334, 390, 330, 220],
+
           }
-  
+
         ]
-  
+
       }
-  
+
     },
-  
-    AGV:{
-  
-      name:"AGV每工位拣货量",
-  
-      data:{
-  
+
+    AGV: {
+
+      name: "AGV每工位拣货量",
+
+      data: {
+
         title: {
-  
+
           text: '当日拣货任务汇总',
-  
+
           subtext: ''
-  
+
         },
-  
-        xAxis:{
-  
-          data:['工作站1', '工作站2', '工作站3', '工作站4', '工作站5', '工作站6', '工作站7'],
-  
+
+        xAxis: {
+
+          data: ['工作站1', '工作站2', '工作站3', '工作站4', '工作站5', '工作站6', '工作站7'],
+
         },
-  
-        series:[
-  
+
+        series: [
+
           {
-  
-            name:'2011',
-  
-            data:[1000, 52, 200, 334, 390, 330, 220],
-  
+
+            name: '2011',
+
+            data: [1000, 52, 200, 334, 390, 330, 220],
+
           },
-  
+
           {
-  
-            name:'2012',
-  
-            data:[10, 52, 200, 334, 390, 330, 220],
-  
+
+            name: '2012',
+
+            data: [10, 52, 200, 334, 390, 330, 220],
+
           }
-  
+
         ]
-  
+
       }
-  
+
     },
-  
+
   }
 
 })
@@ -396,110 +396,135 @@ router.get('/epimetheus/api/diy/report/queryReportByCode/3', async (ctx, next) =
 router.post('/epimetheus/api/diy/report/querySingleReportByCode/', async (ctx, next) => {
   let obj = null;
 
-  
+
   //页面2
-  if ("pickingTaskTotal" == ctx.request.body.code){
+  if ("pickingTaskTotal" == ctx.request.body.code) {
     //当日拣货任务汇总
-    obj=[{
+    obj = [{
       "code": "total",
-      "e":  Math.round(Math.random()*10000),
+      "e": Math.round(Math.random() * 10000),
       "sku": 100000
-      },{
+    }, {
       "code": "finish",
-      "e":  Math.round(Math.random()*10000),
-      "sku":  Math.round(Math.random()*10000)
-      },{
+      "e": Math.round(Math.random() * 10000),
+      "sku": Math.round(Math.random() * 10000)
+    }, {
       "code": "unfinish",
-      "e":  Math.round(Math.random()*10000),
-      "sku":  Math.round(Math.random()*10000)
-      },{
+      "e": Math.round(Math.random() * 10000),
+      "sku": Math.round(Math.random() * 10000)
+    }, {
       "code": "abnormal",
-      "e":  Math.round(Math.random()*10000),
-      "sku":  Math.round(Math.random()*10000)
-      }]
+      "e": Math.round(Math.random() * 10000),
+      "sku": Math.round(Math.random() * 10000)
+    }]
   }
-  if ("DC-Picking" == ctx.request.body.code){
+  if ("DC-Picking" == ctx.request.body.code) {
     //DC拣货任务
-    obj=[{
+    obj = [{
       "code": "total",
       "e": 341,
       "sku": 342
-      },{
+    }, {
       "code": "finish",
       "e": 324,
       "sku": 4324
-      },{
+    }, {
       "code": "unfinish",
       "e": 6,
       "sku": 230
-      },{
+    }, {
       "code": "abnormal",
       "e": 7,
       "sku": 234
-      }]
+    }]
   }
-  if ("AGV-Picking" == ctx.request.body.code){
+  if ("AGV-Picking" == ctx.request.body.code) {
     //AGV拣货任务
-    obj=[{
+    obj = [{
       "code": "total",
       "e": 341,
       "sku": 342
-      },{
+    }, {
       "code": "finish",
       "e": 324,
       "sku": 4324
-      },{
+    }, {
       "code": "unfinish",
       "e": 6,
       "sku": 23
-      },{
+    }, {
       "code": "abnormal",
       "e": 7,
       "sku": 234
-      }]
+    }]
   }
-  if ("DC-Replenish" == ctx.request.body.code){
+  if ("DC-Replenish" == ctx.request.body.code) {
     //DC补货任务
-    obj=[{
+    obj = [{
       "code": "total",
       "e": 341,
       "sku": 342
-      },{
+    }, {
       "code": "finish",
       "e": 324,
       "sku": 432
-      },{
+    }, {
       "code": "unfinish",
       "e": 6,
       "sku": 23
-      },{
+    }, {
       "code": "abnormal",
       "e": 7,
       "sku": 234
-      }]
+    }]
   }
 
 
 
 
   //页面3
-  if ("TrendChart" == ctx.request.body.code){
+  if ("TrendChart" == ctx.request.body.code) {
     //图1
-    obj={
-      a:123
-    }
+    obj = [{
+      "xAxis": "18",
+      "series": 3346
+    }, {
+      "xAxis": "19",
+      "series": 3346
+    }, {
+      "xAxis": "20",
+      "series": 1216
+    }]
   }
-  else if("eachOnePicking" == ctx.request.body.code){
+  else if ("eachOnePicking" == ctx.request.body.code) {
     //图2
-    obj={
-      a:222
-    }
+    obj = [
+      {
+        "xAxis": "甲",
+        "series": 213
+      },
+      {
+        "xAxis": "乙",
+        "series": 321
+      },
+      {
+        "xAxis": "丙",
+        "series": 100
+      }
+    ]
   }
-  else if("eachStation" == ctx.request.body.code){
+  else if ("eachStation" == ctx.request.body.code) {
     //图3
-    obj={
-      a:333
-    }
+    obj = [{
+      "xAxis": "工作站1",
+      "series": 3346
+    }, {
+      "xAxis": "工作站2",
+      "series": 3346
+    }, {
+      "xAxis": "工作站3",
+      "series": 888
+    }]
   }
 
   ctx.body = obj;
